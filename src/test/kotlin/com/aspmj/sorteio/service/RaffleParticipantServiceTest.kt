a19ca969ea3d8969bc07a792d0cc3cbdf835d8bf
felipenathan <felipe.nathan@totvs.com.br>
2021-04-23 11:31:06 -0300

    Refactoring unit test

--- src/test/kotlin/com/aspmj/sorteio/service/RaffleParticipantServiceTest.kt
+++ src/test/kotlin/com/aspmj/sorteio/service/RaffleParticipantServiceTest.kt
@@ -1,12 +1,15 @@
 package com.aspmj.sorteio.service
 
 import com.aspmj.sorteio.MockitoHelper
-import com.aspmj.sorteio.exception.DateLimitExceedException
-import com.aspmj.sorteio.exception.DateLimitStillNotBeginException
+import com.aspmj.sorteio.config.broker.BrokerConstants
+import com.aspmj.sorteio.config.broker.BrokerMessage
 import com.aspmj.sorteio.exception.NoParticipantsException
+import com.aspmj.sorteio.exception.ParticipantAlreadyExistsException
 import com.aspmj.sorteio.exception.ParticipantAlreadyRaffledException
+import com.aspmj.sorteio.exception.RaffleException
 import com.aspmj.sorteio.repository.RaffleParticipantRepository
 import com.aspmj.sorteio.repository.RaffleRepository
+import com.aspmj.sorteio.vo.RaffleParticipantVO
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -15,10 +18,13 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.any
 import org.mockito.Mockito.anyLong
+import org.mockito.Mockito.anyString
 import org.mockito.Spy
 import org.springframework.amqp.core.DirectExchange
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 import kotlin.test.assertEquals
@@ -26,8 +32,6 @@ import kotlin.test.assertNotNull
 
 @ExtendWith(SpringExtension::class)
 class RaffleParticipantServiceTest {
-
-    //TODO REFACTOR TESTS
 
     @Mock
     lateinit var repository: RaffleParticipantRepository
@@ -93,48 +97,6 @@ class RaffleParticipantServiceTest {
         assertEquals(vo.number, participant.number)
     }
 
-//    @Test
-//    fun `when add participant before of begin date then throw DateLimitStillNotBeginException`() {
-//
-//        val participantVO = newParticipantVO()
-//        val raffle = newRaffle(beginDate = tomorrow, endDate = tomorrow, raffleDate = tomorrow)
-//
-//        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
-//
-//        assertThrows<DateLimitStillNotBeginException> { service.sendParticipantToQueue(participantVO) }
-//
-//        Mockito.verify(service, Mockito.never())
-//            .existsByPhoneAndRaffle(Mockito.anyString(), MockitoHelper.anyObject())
-//    }
-
-//    @Test
-//    fun `when add participant after of end date then throw DateLimitExceedException`() {
-//
-//        val participantVO = newParticipantVO()
-//        val raffle = newRaffle(beginDate = yesterday, endDate = yesterday, raffleDate = tomorrow)
-//
-//        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
-//
-//        assertThrows<DateLimitExceedException> { service.sendParticipantToQueue(participantVO) }
-//
-//        Mockito.verify(service, Mockito.never())
-//            .existsByPhoneAndRaffle(Mockito.anyString(), MockitoHelper.anyObject())
-//    }
-
-//    @Test
-//    fun `when add participant after of raffle date then throw DateLimitExceedException`() {
-//
-//        val participantVO = newParticipantVO()
-//        val raffle = newRaffle(beginDate = yesterday, endDate = tomorrow, raffleDate = yesterday)
-//
-//        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
-//
-//        assertThrows<DateLimitExceedException> { service.sendParticipantToQueue(participantVO) }
-//
-//        Mockito.verify(service, Mockito.never())
-//            .existsByPhoneAndRaffle(Mockito.anyString(), MockitoHelper.anyObject())
-//    }
-
     @Test
     fun `when have no participants then throw NoParticipantsException`() {
 
@@ -182,5 +144,55 @@ class RaffleParticipantServiceTest {
         assertEquals(vo.raffledDate, participant.raffledDate)
         assertNotNull(vo.raffledDate)
         assertNotNull(participant.raffledDate)
+    }
+
+    @Test
+    fun `when send right vo to rabbitmq then should not throw any exception`() {
+
+        val participant = newParticipantVO()
+        val response = BrokerMessage(participant)
+
+        `when`(rabbitTemplate.convertSendAndReceiveAsType(
+            Mockito.eq(participantExchange.name),
+            Mockito.eq(BrokerConstants.NEW_PARTICIPANT_ROUTE),
+            MockitoHelper.anyObject(BrokerMessage::class.java),
+            MockitoHelper.anyObject(ParameterizedTypeReference::class.java)
+        )).thenReturn(response)
+
+        assertDoesNotThrow { service.sendParticipantToQueue(participant) }
+    }
+
+    @Test
+    fun `when rabbitmq return null then should not throw Exception`() {
+
+        val participant = newParticipantVO()
+
+        `when`(rabbitTemplate.convertSendAndReceiveAsType(
+            Mockito.eq(participantExchange.name),
+            Mockito.eq(BrokerConstants.NEW_PARTICIPANT_ROUTE),
+            MockitoHelper.anyObject(BrokerMessage::class.java),
+            MockitoHelper.anyObject(ParameterizedTypeReference::class.java)
+        )).thenReturn(null)
+
+        val exception = assertThrows<Exception> { service.sendParticipantToQueue(participant) }
+        assertEquals("Houve uma falha ao adicionar o novo participante", exception.message)
+    }
+
+    @Test
+    fun `when rabbitmq return response with error then should not throw RaffleException with error message`() {
+
+        val participant = newParticipantVO()
+        val thrown = ParticipantAlreadyExistsException()
+        val response = BrokerMessage<RaffleParticipantVO>(error = thrown.message)
+
+        `when`(rabbitTemplate.convertSendAndReceiveAsType(
+            Mockito.eq(participantExchange.name),
+            Mockito.eq(BrokerConstants.NEW_PARTICIPANT_ROUTE),
+            MockitoHelper.anyObject(BrokerMessage::class.java),
+            MockitoHelper.anyObject(ParameterizedTypeReference::class.java)
+        )).thenReturn(response)
+
+        val exception = assertThrows<RaffleException> { service.sendParticipantToQueue(participant) }
+        assertEquals(thrown.message, exception.message)
     }
 }
--- src/test/kotlin/com/aspmj/sorteio/service/RaffleServiceTest.kt
+++ src/test/kotlin/com/aspmj/sorteio/service/RaffleServiceTest.kt
@@ -1,16 +1,22 @@
 package com.aspmj.sorteio.service
 
+import com.aspmj.sorteio.exception.DateLimitExceedException
+import com.aspmj.sorteio.exception.DateLimitStillNotBeginException
 import com.aspmj.sorteio.extension.maxTime
 import com.aspmj.sorteio.model.Raffle
 import com.aspmj.sorteio.repository.RaffleParticipantRepository
 import com.aspmj.sorteio.repository.RaffleRepository
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 import kotlin.test.assertEquals
 
 @ExtendWith(SpringExtension::class)
@@ -42,4 +48,49 @@ class RaffleServiceTest {
         assertEquals(raffle.endDate, vo.endDate.maxTime())
         assertEquals(raffle.raffleDate, vo.raffleDate.maxTime())
     }
+
+    @Test
+    fun `when validate raffle before of begin date then throw DateLimitStillNotBeginException`() {
+
+        val participantVO = newParticipantVO()
+        val raffle = newRaffle(beginDate = tomorrow, endDate = tomorrow, raffleDate = tomorrow)
+
+        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
+
+        assertThrows<DateLimitStillNotBeginException> { raffleService.validateRaffle(participantVO.raffleId!!) }
+    }
+
+    @Test
+    fun `when validate raffle after of end date then throw DateLimitExceedException`() {
+
+        val participantVO = newParticipantVO()
+        val raffle = newRaffle(beginDate = yesterday, endDate = yesterday, raffleDate = tomorrow)
+
+        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
+
+        assertThrows<DateLimitExceedException> { raffleService.validateRaffle(participantVO.raffleId!!) }
+    }
+
+    @Test
+    fun `when validate raffle after of raffle date then throw DateLimitExceedException`() {
+
+        val participantVO = newParticipantVO()
+        val raffle = newRaffle(beginDate = yesterday, endDate = tomorrow, raffleDate = yesterday)
+
+        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
+
+        assertThrows<DateLimitExceedException> { raffleService.validateRaffle(participantVO.raffleId!!) }
+    }
+
+    @Test
+    fun `when validate raffle in range of period then do nothing`() {
+
+        val participantVO = newParticipantVO()
+        val raffle = newRaffle(beginDate = yesterday, endDate = tomorrow, raffleDate = tomorrow)
+
+        `when`(raffleRepository.getOne(UUID.fromString(participantVO.raffleId))).thenReturn(raffle)
+
+        assertDoesNotThrow {  raffleService.validateRaffle(participantVO.raffleId!!) }
+    }
+
 }
